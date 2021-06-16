79a2dae57db18ca5450908219cbae8f7104ea738
eduairet <hola@eduairet.com>
2021-06-16 16:37:27 -0500

    Pre-roulette done

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,16 @@
+const titles = ['Lettering Jam', 'Calligraphy Jam', 'Letter Jam'];
 
+let counter = 0;
+window.setInterval(function(){
+    document.getElementById("page-title").innerText = `${titles[counter % (titles.length)]} ${String.fromCodePoint(0x270D)}`;
+    counter += 1;
+}, 2000);
+
+const letterer = document.getElementById('lname');
+const log = document.getElementById('log');
+
+letterer.addEventListener('change', updateValue, console.log(letterer));
+
+function updateValue(e) {
+  log.textContent = e.target.value;
+}
