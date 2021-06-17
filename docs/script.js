8a65e8e55e3346567af2bc6c59be1ca4f195e669
eduairet <hola@eduairet.com>
2021-06-16 22:19:19 -0500

    Website functional

--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,48 @@
 const titles = ['Lettering Jam', 'Calligraphy Jam', 'Letter Jam'];
+const inputText = document.querySelector('input[type="text"]');
+const letterers = document.getElementById("letterers");
+const letterersElements = document.querySelector('.letterers-element');
+const roll =  document.getElementById('roll')
+
+let words = [ "wear",  "couple",  "feed",  "occupy",  "lie",  "convey",  "locate",  "hold",  "owe",  "cut",  "count",  "double",  "check",  "assign",  "boost",  "appreciate",  "reckon",  "taste",  "employ",  "convict",  "plead",  "widen",  "plant",  "dig",  "prepare",  "decorate",  "descend",  "bind",  "advise",  "behave",  "drown",  "divert",  "create",  "differ",  "condemn",  "creep",  "strike",  "characterise",  "argue",  "describe",  "issue",  "offer",  "bid",  "fine",  "spend",  "start",  "term",  "alter",  "order",  "suffer",];
+let emotions = [ "Sad",  "Happy",  "Naive",  "Serious",  "Scared",  "Angry",  "Hungry",  "Satisfied",  "Excited",  "Focused",  "Confident",  "Lucky",];
 
 let counter = 0;
-window.setInterval(function(){
-    document.getElementById("page-title").innerText = `${titles[counter % (titles.length)]} ${String.fromCodePoint(0x270D)}`;
+setInterval(changeTitle, 2000);
+function changeTitle() {
+    document.getElementById("page-title").textContent = `${titles[counter % (titles.length)]} ${String.fromCodePoint(0x270D)}`;
     counter += 1;
-}, 2000);
+}
 
-const letterer = document.getElementById('lname');
-const log = document.getElementById('log');
+inputText.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+    }
+});
 
-letterer.addEventListener('change', updateValue, console.log(letterer));
-
+roll.addEventListener('click', updateValue);
 function updateValue(e) {
-  log.textContent = e.target.value;
+    e.preventDefault();
+    if (inputText.value) {
+        const lettererElement = document.createElement("DIV");
+        lettererElement.className = 'letterers-element';
+        const lettererElementChild = document.createElement("P");
+        lettererElementChild.className = 'letterers-element-child';
+        lettererElementChild.innerText = inputText.value;
+        lettererElement.appendChild(lettererElementChild);
+        lettererElement.appendChild(elementArray(words));
+        lettererElement.appendChild(elementArray(emotions));
+        letterers.appendChild(lettererElement);
+        inputText.value = null;
+    } else {
+        alert('Capture the letterer\'s name');
+    }
+}
+
+function elementArray(arr) {
+    const lettererElementChild = document.createElement("P");
+    lettererElementChild.className = 'letterers-element-child';
+    const txt = arr[ Math.round(Math.random() * arr.length) ]
+    lettererElementChild.innerText = txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    return lettererElementChild;
 }
